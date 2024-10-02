ce7e486a4cd47bea4df54002fa8974bc6c5f83cc
abdulmoeeddev <abdulmoeed455@gmail.com>
2024-10-01 22:33:58 -0700

    changes

--- React_proj_BGchanger-9/src/App.jsx
+++ React_proj_BGchanger-9/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   };
 
   return (
-    <div style={{ backgroundColor: bgColor, height: '100vh' }}>
-      <button onClick={changeBgColor}>Change Background with multiple Colors</button>
-      <button onClick={() => setBgColor('white')}className="mb-4">Change to white</button>
-      <button onClick={() => setBgColor('orange')} className="mb-2">Change to orange</button>
-      <button onClick={() => setBgColor('black')}className="mb-2">Change to black</button>
-      <button onClick={() => setBgColor('pink')}className="mb-2">Change to pink</button>
-      <button onClick={() => setBgColor('red')}className="mb-2">Change to Red</button>
+    <div style={{ backgroundColor: bgColor, height: '100vh', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'column', paddingBottom: '20px' }}>
+      <button onClick={changeBgColor} style={{ borderRadius: '20px', padding: '10px 20px', margin: '5px' }}>Change Background with multiple Colors</button>
+      <button onClick={() => setBgColor('white')} style={{ borderRadius: '20px', padding: '10px 20px', margin: '5px' }}>Change to white</button>
+      <button onClick={() => setBgColor('orange')} style={{ borderRadius: '20px', padding: '10px 20px', margin: '5px' }}>Change to orange</button>
+      <button onClick={() => setBgColor('black')} style={{ borderRadius: '20px', padding: '10px 20px', margin: '5px' }}>Change to black</button>
+      <button onClick={() => setBgColor('pink')} style={{ borderRadius: '20px', padding: '10px 20px', margin: '5px' }}>Change to pink</button>
+      <button onClick={() => setBgColor('red')} style={{ borderRadius: '20px', padding: '10px 20px', margin: '5px' }}>Change to Red</button>
     </div>
   );
 }
